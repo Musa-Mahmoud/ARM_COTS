3e3e19348cf51a1b1526b176798afbeeafdc78b6
Musa Mahmoud <mosa.mahmoud87@gmail.com>
2023-08-06 06:07:47 +0300

    Feature: add stm32446re SYSCFG driver

--- stm32f446re/1-MCAL/7-SYSCFG/Inc/SYSCFG_prv.h
+++ stm32f446re/1-MCAL/7-SYSCFG/Inc/SYSCFG_prv.h
@@ -1,6 +1,9 @@
 #ifndef SYSCFG_RRV_H_
 #define SYSCFG_RRV_H_
 
+#define EXTI_CTRL_REG_ACCESS            (4U)
+#define EXTI_CTRL_BITS_ACCESS           (4U)
 
+#define EXTICR_MASK                     (0xFU)
 
 #endif /* SYSCFG_PRV_H_ */
--- stm32f446re/1-MCAL/7-SYSCFG/Src/SYSCFG_prog.c
+++ stm32f446re/1-MCAL/7-SYSCFG/Src/SYSCFG_prog.c
@@ -17,4 +17,21 @@
 #include "SYSCFG_interface.h"
 #include "SYSCFG_prv.h"
 
+uint8_t SYSCFG_u8SetEXTIPort(SYSCFG_LineNumber_t Copy_LineNumber, SYSCFG_Port_t Copy_Port)
+{
+    uint8_t Local_u8RegNum = Copy_LineNumber / EXTI_CTRL_REG_ACCESS;
+    uint8_t Local_u8BitNum = (Copy_LineNumber % EXTI_CTRL_BITS_ACCESS) * EXTI_CTRL_BITS_ACCESS;
 
+    if (Copy_LineNumber > SYSCFG_RTC_WKUP_LINE || Copy_LineNumber < SYSCFG_EXTI0_LINE)
+    {
+        return NOK;
+    }
+
+    /* Clear port number */
+    SYSCFG->EXTICR[Local_u8RegNum] &= (EXTICR_MASK) << Local_u8BitNum;
+
+    /* Set port number */
+    SYSCFG->EXTICR[Local_u8RegNum] |= Copy_Port << Local_u8BitNum;
+
+    return OK;
+}
