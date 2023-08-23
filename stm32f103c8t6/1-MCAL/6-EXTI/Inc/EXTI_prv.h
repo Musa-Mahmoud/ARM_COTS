f5635cc21debf605505e9ae290de43ce4729a643
Musa Mahmoud <mosa.mahmoud87@gmail.com>
2023-08-23 19:32:26 +0300

    Feature: add stm32f103c8t6 EXTI driver

---
+++ stm32f103c8t6/1-MCAL/6-EXTI/Inc/EXTI_prv.h
@@ -0,0 +1,9 @@
+#ifndef EXTI_RRV_H_
+#define EXTI_RRV_H_
+
+#define LINES_NUMBER            (19U)
+
+#define FLAG_DOWN               (0U)
+#define FLAG_RAISED             (1U)
+
+#endif /* EXTI_PRV_H_ */
